198e9d32d32fadbbb1743f977ed726c496684da7
Girish <pgirisha77@gmail.com>
2020-03-11 01:00:10 +0530

    First commit

---
+++ src/test/java/com/example/todoapigateway/TodoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
